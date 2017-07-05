535ed1d66e2e55518b0f65ec2ccbe38cb539dc2a
Exybore <exybore@becauseofprog.fr>
2017-07-05 16:01:23 +0200

    Full system and design

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# dav-dance
+# Just Dance Random - Main repository
+Just Dance Random allows you to choose random names and a song to play it in Just Dance. Made by Exybore for DavAll.
--- script.js
+++ script.js
@@ -3,7 +3,115 @@ window.onload=function(){
 	var paragraph = document.getElementById('paragraph');
 	var form = document.getElementById('players-form');
 
-	
+}
+
+function randomVideo(number){
+
+	switch (number) {
+		case 0:
+			var videos = ["Pt_G2cw-5qI",
+			"QoYOWKkXsck",
+			"JdEzwBjyI9w",
+			"3-uSqERMUss",
+			"nmDHgnt9n6o",
+			"OuwxEBl4hyc",
+			"7FRt6q0Pzfc",
+			"MDL0zZydn2Q",
+			"KnOnK4bg2LY",
+			"JTmb5Kp3gqY",
+			"sf8C6-EIrWk",
+			"BQLcgWSnNdM",
+			"KCecBUpcTT0",
+			"nToEPVE2Hno",
+			"pfvMecHT7bI",
+			"BieKXHnpieE",
+			"shf6H_B3y1I",
+			"bhiLSeRq9jI",
+			"I7Kta8YSkdQ",
+			"NZJ2mr0gWlY",
+			"jrflvDj1w_A",
+			"oG474xdJt6E",
+			"atksnGtmvPM",
+			"Lc5tANkbetY",
+			"xez5qgRNrNE",
+			"744ga2QpahQ",
+			"SSK8JfpIrbU",
+			"ucKE-7Xg2TU",
+			"MtLtabuzyVY",
+			"OuTdAg381zw",
+			"ak3Zs43Fv4c",
+			"pmueY9AzW38",
+			"itr7k2HXlHw",
+			"a0wnxVEV_XY",
+			"n3hsNlj_KzQ",
+			"7ObOM_g5Ot0",
+			"rQbE5NnnWhI",
+			"5dpWpyfkeFo",
+			"4n8nE29hSvg",
+			"kXBUY0SCNp0"];
+			return videos[aleatoire(videos.length)];
+
+		case 1:
+			var videos = ["xsBNppuVVBo",
+			"Fc-ML9MLQdQ",
+			"cCtYbwTKeyk",
+			"NP-rkhUgS6M",
+			"s-PN1P_7ECU",
+			"2T4fDjwSEPM",
+			"a0xUqX-eQCQ",
+			"p96A4mnfY0k",
+			"lkCIB9h2EVQ",
+			"s0rtGwLeFAw",
+			"2o_45G86SGQ",
+			"x1EHKtoPbEQ",
+			"tQyiAsQ6m4c",
+			"N0mkZ0P7gxU",
+			"P5xcqcCiu2o",
+			"4ch14ORtM3A",
+			"Aq7Ns5J2Zvw",
+			"4XcGPSc48pw",
+			"HKaGXvUure0",
+			"Y-4s9GS2Zmk"];
+			return videos[aleatoire(videos.length)];
+
+		case 2:
+			var videos = ["YXUO_3VGw2M",
+			"fVMZPQ93PVg",
+			"WkzQxaMPve0",
+			"lXvoS3QQLC4",
+			"_DDOGfuDNkM",
+			"Tq_GYNi15Sw",
+			"g3CMAJFwWoI",
+			"7ZGsdigUX4E",
+			"n9XTdGFniWM",
+			"api-BeryYSM",
+			"MDlcAR911WM",
+			"tUFV-K_MzZs"];
+			return videos[aleatoire(videos.length)];
+
+		case 3:
+			var videos = ["tclPEggpgPk",
+			"h7R5IP8sqYI",
+			"pV-25QUrF0c",
+			"Nt-0yK9gPv4",
+			"Lhjs_qFxLXs",
+			"KbWBuZcZN2s",
+			"L_z4mmN-T3Q",
+			"sxdmPwl6jcA",
+			"y7KOPoOPNyc",
+			"GJxkmeysy6s",
+			"6Nxq87Lk5DE",
+			"r7YWe8CCMiY",
+			"9W8CLdJ5Ft4",
+			"PgMx6jr7RjY",
+			"olv9jsDqsAY"];
+			return videos[aleatoire(videos.length)];
+
+		default:
+			return "55VPB9qnhW8";
+
+	}
+
 }
 
 function aleatoire(number) { 
@@ -47,23 +155,16 @@ function determineType (number) {
 
 function valid () {
 	document.getElementById('players-form').style.display = "none";
-	document.getElementById('final-div').style.display = "block";
+	document.getElementById('final-div').style.display = "flex";
 
 	var players = document.getElementById('input-players').value.split(";")
-	players.forEach(function(element) {
-		document.getElementById('paragraph').innerHTML += element;
-		document.getElementById('paragraph').innerHTML += " - ";
-	});
-
-	document.getElementById('paragraph').innerHTML += toUse;
-	document.getElementById('paragraph').innerHTML += " - ";
-	document.getElementById('paragraph').innerHTML += players.length;
 
 	switch (determineType(players.length)) {
 		case 0:
 			
 			var toUse = aleatoire(players.length);
-			document.getElementById('role').innerHTML += players[toUse]+' - Solo';
+			document.getElementById('role').innerHTML = players[toUse]+' - Solo';
+			document.getElementById('dance').innerHTML = "<iframe frameBorder=\"0\" src=\"https://www.youtube-nocookie.com/embed/"+randomVideo(0)+"\">Navigateur non compatible.</iframe>";
 			break;
 
 		case 1:
@@ -75,7 +176,8 @@ function valid () {
 				var toUseTwo = aleatoire(players.length);
 			}
 
-			document.getElementById('role').innerHTML += players[toUse]+' et '+players[toUseTwo]+' - Duo';
+			document.getElementById('role').innerHTML = players[toUse]+' et '+players[toUseTwo]+' - Duo';
+			document.getElementById('dance').innerHTML = "<iframe frameBorder=\"0\" src=\"https://www.youtube-nocookie.com/embed/"+randomVideo(1)+"\">Navigateur non compatible.</iframe>";
 
 			break;
 
@@ -90,11 +192,12 @@ function valid () {
 
 			var toUseThree = toUseTwo;
 
-			while ((toUseThree != toUse) && (toUseThree == toUseTwo)) {
+			while ((toUseThree == toUse) |- (toUseThree == toUseTwo)) {
 				var toUseThree = aleatoire(players.length);
 			}
 
-			document.getElementById('role').innerHTML += players[toUse]+', '+players[toUseTwo]+' et '+players[toUseThree]+' - Trio';
+			document.getElementById('role').innerHTML = players[toUse]+', '+players[toUseTwo]+' et '+players[toUseThree]+' - Trio';
+			document.getElementById('dance').innerHTML = "<iframe frameBorder=\"0\" src=\"https://www.youtube-nocookie.com/embed/"+randomVideo(2)+"\">Navigateur non compatible.</iframe>";
 
 			break;
 
@@ -109,17 +212,18 @@ function valid () {
 
 			var toUseThree = toUseTwo;
 
-			while (toUseThree != toUse && toUseThree == toUseTwo) {
+			while (toUseThree == toUse || toUseThree == toUseTwo) {
 				var toUseThree = aleatoire(players.length);
 			}
 
 			var toUseFour = toUseThree;
 
-			while (toUseFour != toUse && toUseFour != toUseTwo && toUseFour == toUseThree) {
+			while (toUseFour == toUse || toUseFour == toUseTwo || toUseFour == toUseThree) {
 				var toUseFour = aleatoire(players.length);
 			}
 
-			document.getElementById('role').innerHTML += players[toUse]+', '+players[toUseTwo]+', '+players[toUseThree]+' et '+players[toUseFour]+' - Quatro';
+			document.getElementById('role').innerHTML = players[toUse]+', '+players[toUseTwo]+', '+players[toUseThree]+' et '+players[toUseFour]+' - Quatro';
+			document.getElementById('dance').innerHTML = "<iframe frameBorder=\"0\" src=\"https://www.youtube-nocookie.com/embed/"+randomVideo(3)+"\">Navigateur non compatible.</iframe>";		
 
 			break;
 
